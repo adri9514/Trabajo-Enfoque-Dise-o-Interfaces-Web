43c67ccb0c2662d9f92db5152e79071d3856a510
adri9514 <adriexposito9514@gmail.com>
2024-11-05 15:05:15 +0100

    web definitiva

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,7 +1,10 @@
-function mostrarDescripcion() {
-    document.querySelector('.descripcion').style.display = 'block';
-}
+// Selecciona todos los productos destacados y agrega eventos de ratón
+document.querySelectorAll('#destacados .producto').forEach((producto) => {
+    producto.addEventListener('mouseenter', () => {
+        producto.querySelector('.descripcion').style.display = 'block';
+    });
 
-function ocultarDescripcion() {
-    document.querySelector('.descripcion').style.display = 'none';
-}
+    producto.addEventListener('mouseleave', () => {
+        producto.querySelector('.descripcion').style.display = 'none';
+    });
+});
